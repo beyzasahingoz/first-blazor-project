33e0e6986a20705116ec92464baccb0e9bc061c6
bfalconeye <70069234+bfalconeye@users.noreply.github.com>
2022-07-11 21:21:47 +0300

    Files upload

---
+++ ToDoListApp/Shared/Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp.Shared.Models
+{
+    public class TaskModel
+    {
+        public  string TaskName { get; set; }
+        public  bool isComplete { get; set; }
+        
+    }
+}
